bb90754667d920f7edb874e5a0589593775e0b2a
bernouts <shakozz@live.com>
2020-09-18 16:23:15 -0500

    initial commit

---
+++ IconPicker/IconPicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IconPicker
+//
+//  Created by Brenden Smith on 9/18/20.
+//  Copyright © 2020 Brenden Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
